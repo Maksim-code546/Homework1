2e8b89d7092da11f1ef85414abf4b232588b7b8b
maksim-code546 <maksimzubov709@gmail.com>
2025-04-18 22:02:06 +0500

    task5

--- Script.js
+++ Script.js
@@ -1,11 +1,12 @@
-//task4
+//task5
 
-const obj = {
-    "Коля" : '200',
-    "Вася" : '300',
-    "Петя" : '400'
-    };
-    
-    for (const name in obj) {
-        console.log(`${name} - Зарплата ${obj[name]} долларов`);
-    }
+let n = 1000;
+let num = 0;
+
+while (n >= 50) {
+    n = n / 2;
+    num++;
+}
+
+console.log("Итоговое число:", n);
+console.log("Количество итераций:", num);
