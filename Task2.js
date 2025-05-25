9abb0ebc0ed3e9a4d5ef26c65afb231b0daa7114
maksim-code546 <maksimzubov709@gmail.com>
2025-05-25 23:17:31 +0500

    task3

--- Task2.js
+++
@@ -1,7 +0,0 @@
-//task2
-
-const masiv = [1, 5, 4, 10, 0, 3];
-const index = masiv.indexOf(4);
-
-console.log(index);
- 
---
+++ Task3.js
@@ -0,0 +1,8 @@
+//task3
+
+let numbs = [1, 3, 5, 10, 20];
+let result = numbs.join(' ');
+
+console.log(result);
+
+ 
