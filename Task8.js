7606c2aeca2815d76618537e71a81fb76203319a
maksim-code546 <maksimzubov709@gmail.com>
2025-05-26 06:55:39 +0500

    task9

--- Task8.js
+++
@@ -1,5 +0,0 @@
-//task8
-
-const result = 'abcdef';
-const reversResult = result.split('').reverse().join('');
-console.log(reversResult);
---
+++ Task9.js
@@ -0,0 +1,6 @@
+//task9
+
+const result = [[1, 2, 3],[4, 5, 6]];
+
+const flatArray = [...result[0], ...result[1]];
+console.log(flatArray);
