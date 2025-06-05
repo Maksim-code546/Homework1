3c2f86728e83afbbb1d6f7035daf213f88a333bc
maksim-code546 <maksimzubov709@gmail.com>
2025-06-05 07:14:24 +0500

    task2

--- embedded objects.js
+++ embedded objects.js
@@ -4,3 +4,16 @@ const str = 'js';
 const upperStr = str.toUpperCase();
 
 console.log(upperStr);
+
+//task2
+
+function filterStringsStartingWith(array, str) {
+  const lowerStr = str.toLowerCase();
+  return array.filter(item => 
+    item.toLowerCase().startsWith(lowerStr)
+  );
+}
+
+const fruits = ['apple', 'Banana', 'grape', 'apricot', 'Pineapple'];
+const result = filterStringsStartingWith(fruits, 'ap');
+console.log(result);
