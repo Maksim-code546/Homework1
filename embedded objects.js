2bddb69f9804b161f26889d63ab5bf995413755d
maksim-code546 <maksimzubov709@gmail.com>
2025-06-05 07:28:38 +0500

    task3

--- embedded objects.js
+++ embedded objects.js
@@ -17,3 +17,11 @@ function filterStringsStartingWith(array, str) {
 const fruits = ['apple', 'Banana', 'grape', 'apricot', 'Pineapple'];
 const result = filterStringsStartingWith(fruits, 'ap');
 console.log(result);
+
+//task3
+
+let methodMath = 32.58884;
+
+console.log(Math.floor(methodMath));
+console.log(Math.ceil(methodMath));
+console.log(Math.round(methodMath));
