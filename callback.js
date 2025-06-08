06c213a1c8558e849fe629a30fad33a9433653d4
maksim-code546 <maksimzubov709@gmail.com>
2025-06-08 10:06:52 +0500

    task2

--- callback.js
+++ callback.js
@@ -8,3 +8,34 @@ const people = [
 ];
 
 console.log(people.sort((a, b) => a.age - b.age));
+
+//task2
+
+function isPositive(num) {
+  return num > 0;
+}
+
+function isMale(person) {
+  return person.gender === 'male';
+}
+
+function filter(array, ruleFunction) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (ruleFunction(array[i])) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const peopleTestTask2 = [
+  {name: 'Глеб', gender: 'male'},
+  {name: 'Анна', gender: 'female'},
+  {name: 'Олег', gender: 'male'},
+  {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(peopleTestTask2, isMale));
