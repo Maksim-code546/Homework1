e3383076d38619a102f548dcab70d42336ccd712
maksim-code546 <maksimzubov709@gmail.com>
2025-06-10 18:04:07 +0500

    task1

--- DOM.js
+++ DOM.js
@@ -1,1 +1,8 @@
 //task1
+
+        const headingEl = document.querySelector('.heading');
+        const btnEl = document.querySelector('.btn');
+        
+        btnEl.addEventListener('click', () => {
+            headingEl.textContent = 'Привет мир!'
+        })
